3c8424fd611fe5f08ce983b0a43b6aa2a338b7a7
JustZS <96547695+JustZS@users.noreply.github.com>
2023-03-05 15:21:24 -0800

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -9,15 +9,20 @@ if status is-interactive
     fish_add_path /home/justzs/.local/bin
     fish_add_path /home/justzs/.cargo/bin
     source ~/.bash_aliases
-    cat ~/.cache/wal/sequences
+    # cat ~/.cache/wal/sequences
     export EDITOR=nvim
     abbr --add SP sudo pacman -Syu 
     abbr --add .. cd ..
     abbr --add nvw nvim worksheet.tex
     abbr --add zaw fzathura worksheet.pdf
+    abbr --add sudo doas 
+    abbr --add dwmwalclr ./Scripts/dwmwalclr.sh
+    abbr --add volume pavucontrol
+    abbr --add wificonnect nmcli device wifi connect
     abbr --add wifilist nmcli device wifi list
-    abbr --add sudo doas
-    abbr --add wificonnect nmcli device wifi connect THEWIFINETWORK password THEWIFIPASSWORD
     fish_add_path /home/justzs/.local/share/gem/ruby/3.0.0/bin
+    fish_add_path /usr/lib/R/
     set fish_greeting ""
+    bash ./Scripts/startx.sh
+    set -g fish_color_normal "#06080B"
     end
